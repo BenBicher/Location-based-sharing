60e5796bece8485149359f4079732b389a495372
BenBicher <38686322+BenBicher@users.noreply.github.com>
2018-07-02 14:59:38 +0300

    Map bugFix

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import {  } from '@types/googlemaps';
+// import { } from '@types/googlemaps';
 
 @Component({
   selector: 'app-map',
@@ -8,7 +8,7 @@ import {  } from '@types/googlemaps';
 })
 export class MapComponent {
  
-  @ViewChild('gmap') gmapElement: any;
+  // @ViewChild('gmap') gmapElement: any;
   map: google.maps.Map;
 
   isTracking = false;
@@ -24,7 +24,7 @@ export class MapComponent {
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+    // this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
   }
 
   findMe() {
