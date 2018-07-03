f08de92d02ce40909df77871f80d0e0f5332eff5
BenBicher <38686322+BenBicher@users.noreply.github.com>
2018-07-03 13:44:39 +0300

    Export map to a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,32 +5,4 @@ import { Component, ViewChild } from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
 })
-export class AppComponent {
-
-  title: string = 'Location Base Sharing';
-  
-  ngOnInit() {
-    var mapProp = {
-      // center: new google.maps.LatLng(31.771959, 35.217018),
-      
-      // mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-  }
-  zoom: number = 5;
-  lat: number = 31.771959;
-  lng: number = 35.217018;
-  findMe() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        this.showPosition(position);
-      });
-    } else {
-      alert("Geolocation is not supported by this browser.");
-    }
-  }
-  showPosition(position) {
-    this.lat = position.coords.latitude;
-    this.lng = position.coords.longitude;
-    this.zoom = 15;
-  }
-}
+export class AppComponent {}
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -8,25 +8,18 @@ import { Component, ViewChild } from '@angular/core';
 })
 export class MapComponent {
  
-  // @ViewChild('gmap') gmapElement: any;
-  map: google.maps.Map;
-
-  isTracking = false;
-
-  currentLat: any;
-  currentLong: any;
-
-  marker: google.maps.Marker;
-
+  title: string = 'Location Base Sharing';
+  zoom: number = 5;
+  lat: number = 31.771959;
+  lng: number = 35.217018;
+  
   ngOnInit() {
     var mapProp = {
-      center: new google.maps.LatLng(18.5793, 73.8143),
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+      // center: new google.maps.LatLng(31.771959, 35.217018),
+      
+      // mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    // this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
   }
-
   findMe() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
@@ -36,54 +29,9 @@ export class MapComponent {
       alert("Geolocation is not supported by this browser.");
     }
   }
-
   showPosition(position) {
-    this.currentLat = position.coords.latitude;
-    this.currentLong = position.coords.longitude;
-
-    let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    this.map.panTo(location);
-
-    if (!this.marker) {
-      this.marker = new google.maps.Marker({
-        position: location,
-        map: this.map,
-        title: 'Got you!'
-      });
-    }
-    else {
-      this.marker.setPosition(location);
-    }
-  }
-
-  trackMe() {
-    if (navigator.geolocation) {
-      this.isTracking = true;
-      navigator.geolocation.watchPosition((position) => {
-        this.showTrackingPosition(position);
-      });
-    } else {
-      alert("Geolocation is not supported by this browser.");
-    }
-  }
-
-  showTrackingPosition(position) {
-    console.log(`tracking postion:  ${position.coords.latitude} - ${position.coords.longitude}`);
-    this.currentLat = position.coords.latitude;
-    this.currentLong = position.coords.longitude;
-
-    let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    this.map.panTo(location);
-
-    if (!this.marker) {
-      this.marker = new google.maps.Marker({
-        position: location,
-        map: this.map,
-        title: 'Got you!'
-      });
-    }
-    else {
-      this.marker.setPosition(location);
-    }
+    this.lat = position.coords.latitude;
+    this.lng = position.coords.longitude;
+    this.zoom = 15;
   }
 }
